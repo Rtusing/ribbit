68a9253ddd6b60b4c8a741a44031c3b02642b86e
Ryan Tusing <rtusing@gmail.com>
2017-04-26 15:50:47 +0000

    updated GUI on Home/Profile pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
   #back-end code for pages/home
   def home
      @posts = Post.all
+     @newPost = Post.new
   end
 
   #back-end code for pages/profile
@@ -21,10 +22,13 @@ class PagesController < ApplicationController
     end
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new
+    
+    @toFollow = User.all.last(5)
   end
 
   #back-end code for pages/explore
   def explore
     @posts = Post.all
+    @newPost = Post.new
   end
 end
